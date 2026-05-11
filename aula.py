8b8b9bf6a8e999204046665d0cc3566abcbae2fb
gustavo55100 <gustavo.morais.lara@escola.pr.gov.br>
2026-05-11 10:40:55 -0300

    Create aula.py

---
+++ aula.py
@@ -0,0 +1,6 @@
+nome = str(input("Digite seu nome completo")).strip()
+dividido nome.split()
+print("Muito prazer em te conhecer!")
+print("Seu primeiro nome é ()".format(dividido[0]))
+print("Seu ultimo nome é {}".format(dividido len(dividido)-1]))
+11 de mai. de 2026 às 10:37.png
