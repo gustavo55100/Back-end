2896c7fe378260b2edda7ebb15f32824d2b6f5d7
gustavo55100 <gustavo.morais.lara@escola.pr.gov.br>
2026-05-18 10:50:34 -0300

    Create aula24.py

---
+++ aula24.py
@@ -0,0 +1,19 @@
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o segundo número: "))
+n3 = float(input("Digite o terceiro número: "))
+
+maior = n1
+menor = n1
+
+if n2 > maior:
+    maior = n2
+if n3 > maior:
+    maior = n3
+
+if n2 < menor:
+    menor = n2
+if n3 < menor:
+    menor = n3
+
+print("O maior valor digitado foi {}".format(maior))
+print("O menor valor foi {}".format(menor))
